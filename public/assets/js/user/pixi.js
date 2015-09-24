868ad8dfa4e06e7343d6940e6a49899e3e67a33b
Charlie Clark <clarkcharlie03@gmail.com>
2015-09-24 13:58:29 -0400

    vector 2

--- public/assets/js/user/pixi.js
+++ public/assets/js/user/pixi.js
@@ -1,6 +1,8 @@
 var stage, renderer;
 var mouseX, mouseY;
 
+import Vector2 from "Vector2";
+
 function init(el) {
 
 	renderer = PIXI.autoDetectRenderer( window.innerWidth, window.innerHeight, {
@@ -13,7 +15,19 @@ function init(el) {
 
 	window.addEventListener("resize", resize);
 	window.addEventListener("mousemove", mousemove);
+
+	start();
+
 	animate();
+}
+
+function start(){
+	var node = new Node({
+		x : window.innerWidth / 2,
+		y : window.innerHeight / 2,
+	});
+
+	stage.addChild(node.group);
 }
 
 function mousemove(e) {
@@ -32,6 +46,33 @@ function animate() {
 
 function render() {
 	renderer.render( stage );
+}
+
+//
+class Node{
+	constructor({x, y}){
+		this.position = new Vector2(x,y);
+		this.group = new PIXI.Container();
+		this.init();
+	}
+
+	init(){
+		this.group.addChild(this.circle());
+		this.update();
+	}
+
+	update(){
+		this.group.position.set( this.position.x, this.position.y );
+	}
+
+	circle(){
+		// create a new graphics object
+		var graphics = new PIXI.Graphics();
+		graphics.beginFill(0xffffff);
+		graphics.drawCircle(0, 0, 3);
+		graphics.endFill();
+		return graphics;
+	}
 }
 
 export default {
